80519373a5ed8c9faa1e6793978fb023ea4000ba
Tomas Persson <hypotesfabriken@gmail.com>
2022-11-17 12:29:57 +0100

    Lite förslag
    Lekte lite på lunchen! :) Skapade dels en json med alla events, så blir det enklare att ändra saker (kod på bara ett ställe).
    Fixade även relativa sökvägar så att det funkar lite mer generiskt (om webbplatsen inte kör direkt i rooten / )

--- script.js
+++ script.js
@@ -15,4 +15,36 @@ function infoFunction (event) {
     
 }
 
-infoBtn1.addEventListener('click', infoFunction);
+//infoBtn1.addEventListener('click', infoFunction);
+
+
+function toggle(elem) {
+    var x = elem.nextElementSibling;
+    console.log(x.style.display);
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+$(function() {
+
+    var events = [];
+ 
+    $.getJSON('events.json', function(data) {
+        $.each(data.events, function(i, f) {
+           var tblRow = "<div id='event-"+f.id + "' class='event-card'>" + 
+           "<img alt='" + f.alt + "' class='img' src='" + f.image + "' />" + 
+           "<h2 class='name'>" + f.name + "</h2>" +
+           "<span class='desc'>" + f.desc + "</span>" +
+           "<span class='vategory'>" + f.category + "</span>" +
+           "<button onclick='toggle(this);'>More info</button>" +
+           "<span class='info'>" + f.info + "</span>" +
+           "</div>";
+           $(tblRow).appendTo(".event");
+      });
+ 
+    });
+ 
+ });
