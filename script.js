aa777064d1c84c11aa4a296992b2c89560525b1d
sofiaCoder <sofia.laas@gmail.com>
2022-11-18 14:09:36 +0100

    Fixat och testar js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 let info1 = document.getElementById('info1');
 let infoBtn1 = document.getElementById('infoBtn1');
+let hideBtn = document.getElementById('hideBtn');
 
 function infoFunction (event) {
     info1.style= 'display: block;'
-
-    function hideFunction (event) {
-        info1.style = 'display: none;'
-        hideBtn.remove();
-    }
-    let hideBtn = document.createElement('button')
-    hideBtn.textContent = 'Hide info';
-    info1.appendChild(hideBtn)
-    hideBtn.addEventListener('click', hideFunction)
-    
+    hideBtn.style= 'display: block;'
+}
+function hideFunction (event) {
+    info1.style = 'display: none;'
+    hideBtn.style = 'display: none;'
 }
 
 infoBtn1.addEventListener('click', infoFunction);
+<<<<<<< Updated upstream
 
 function toggle(elem) {
     var x = elem.nextElementSibling;
@@ -28,3 +25,12 @@ function toggle(elem) {
 
     }
   }
+=======
+hideBtn.addEventListener('click', hideFunction)
+
+
+// let hideBtn = document.createElement('button')
+// hideBtn.textContent = 'Hide info';
+// info1.appendChild(hideBtn)
+// hideBtn.addEventListener('click', hideFunction)
+>>>>>>> Stashed changes
