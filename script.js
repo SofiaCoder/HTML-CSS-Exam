77f21644f91163747390491a36121a5b1befffe7
sofiaCoder <sofia.laas@gmail.com>
2022-11-21 23:30:56 +0100

    Fix av form-sidan
    Info-knappen ej fixad
    Form-sidan fixad m grid

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ let infoBtns = document.querySelectorAll('.infoBtn');
 let homePage = document.querySelector('.home');
 let aboutSubmit = document.querySelector('#contact-submit');
 let aboutForm = document.querySelector('#about-form');
+let info = document.querySelectorAll('.info');
+let style = info.style;
 
-console.log(infoBtns);
+
 function infoFunction (event) {
     const text = event.target.nextElementSibling;
     
     if (text.style.display == 'none') {
     text.style.display = 'block';
-    event.target.textContent = 'Hide info'
+    event.target.textContent = 'Hide info';
     } else {
-        event.target.textContent = 'More info'
+        console.log(text.style);
+        event.target.textContent = 'More info';
+        text.style.display = 'none';
+    }
+}
+
+function infoToggle (event) {
+    const text = event.target.nextElementSibling;
+    
+    if (text.style.display == 'none') {
+    text.style.display = 'block';
+    event.target.textContent = 'Hide info';
+    } else {
+        console.log(text.style);
+        event.target.textContent = 'More info';
         text.style.display = 'none';
     }
 }
@@ -22,8 +38,8 @@ infoBtns.forEach(btn => {
 
 
 function submitMessage(event) {
-    aboutForm.innerHTML = '<p>Thank you for contacing us! We will get back to you as soon as possible!</p><img src="/img/dog.jpg">'
-    aboutForm.className = 'about-form-answer'
+    aboutForm.innerHTML = '<p>Thank you for contacing us! We will get back to you as soon as possible!</p><img src="/img/dog.jpg">';
+    aboutForm.className = 'about-form-answer';
 
     
     event.preventDefault();
